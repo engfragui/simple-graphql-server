df8452d5ac3f817f60ce507472e96a0b453376d4
Francesca Guiducci <fguiducci@trulia.com>
2017-10-15 19:00:08 -0700

    Renaming

--- run_mock_api.sh
+++ run_mock_api.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar mock-apis/target/mock-api-*-jar-with-dependencies.jar server mock-api/app-config-local.yml
+java -jar mock-api/target/mock-api-*-jar-with-dependencies.jar server mock-api/app-config-local.yml
